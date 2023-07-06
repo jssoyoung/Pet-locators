1f1b3c80f9c3020b602d03e346c77aea1c1a9885
Young Jang <jyoungjoon@gmail.com>
2023-07-06 01:05:03 -0700

    CHANGE session routes

--- utils/session.js
+++
@@ -1,5 +0,0 @@
-var express = require('express');
-var session = require('express-session');
-var SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-module.exports = SequelizeStore;
